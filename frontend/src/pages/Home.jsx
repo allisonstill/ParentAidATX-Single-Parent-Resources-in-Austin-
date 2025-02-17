3d94fd96cc338a2ed17b2b3cd227c69641c76562
ethany04 <ethan.yu@utexas.edu>
2025-02-17 15:45:42 -0600

    added card map from data array

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import CustomCard from "../components/Card"
-function Home(){
-    return(
+import CustomCard from "../components/Card";
+
+const cardData = [
+  { id: 1, title: "andrew harvey", description: "hello world" },
+  { id: 2, title: "card2", description: "this is the second card" },
+  { id: 3, title: "card3", description: "this is the third card" },
+];
+
+function Home() {
+  return (
     <>
-        <h1>Home page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <CustomCard/>
-    </>);
+      <h1>Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      {cardData.map((currCard) => (
+        <CustomCard title={currCard.title} description={currCard.description} />
+      ))}
+    </>
+  );
 }
 
 export default Home;

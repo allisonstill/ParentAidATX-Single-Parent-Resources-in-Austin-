5105a7abb62e52ada0925077d723d13134dc46f2
allistill12 <allistill12@gmail.com>
2025-02-16 22:14:07 -0600

    Starting to build About page on frontend

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,57 @@
 function About(){
+    const teamMembers = [
+        {
+            name: "",
+            gitid: [""],
+            photo: "",
+            bio: "",
+            responsibilities: "",
+            commits: 0,
+            issues: 0,
+            unit_tests: 0,
+        },
+        {
+            name: "",
+            gitid: [""],
+            photo: "",
+            bio: "",
+            responsibilities: "",
+            commits: 0,
+            issues: 0,
+            unit_tests: 0,
+        },
+        {
+            name: "",
+            gitid: [""],
+            photo: "",
+            bio: "",
+            responsibilities: "",
+            commits: 0,
+            issues: 0,
+            unit_tests: 0,
+        },
+        {
+            name: "",
+            gitid: [""],
+            photo: "",
+            bio: "",
+            responsibilities: "",
+            commits: 0,
+            issues: 0,
+            unit_tests: 0,
+        },
+        {
+            name: "",
+            gitid: [""],
+            photo: "",
+            bio: "",
+            responsibilities: "",
+            commits: 0,
+            issues: 0,
+            unit_tests: 0,
+        },
+    ];
+
     return(
         <>
             <h1>About page</h1>

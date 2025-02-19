b016ff7b35f264add719b7118d99d31fbc46c58f
Rubi Rojas <rubirojas@Rubis-Laptop.local>
2025-02-19 17:56:44 -0600

    housing cards yay

---
+++ frontend/src/components/housingCard.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "./housingCard.css";
+
+function HousingCard({ image, name, cost, rating, HousingStyle, Address, id}) {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+  
+  return (
+    <div className="HousingCard" style={{ width: "25rem" }}>
+      <img
+        src={image}
+        className="HousingCard-img-top"
+        alt=""
+      />
+      <div className="card-body">
+        <h5 className="HousingCard-title">{name}</h5>
+        <p className="attribute-label">Cost</p>
+        <p className="HousingCard-attributes">{cost}</p>
+        <p className="attribute-label">Rating</p>
+        <p className="HousingCard-attributes">{rating}</p>
+        <p className="attribute-label">Housing Style</p>
+        <p className="HousingCard-attributes">{HousingStyle}</p>
+        <p className="attribute-label">Address</p>
+        <p className="HousingCard-attributes">{Address}</p>
+        <Link to={`/programs/${id}`} className="custom-btn" onClick={handleClick}>
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default HousingCard;
--- frontend/src/pages/Housing.jsx
+++ frontend/src/pages/Housing.jsx
@@ -1,21 +1,62 @@
-import GeneralPage from "./GeneralPage"
+import HousingCard from "../components/housingCard";
+import "./Housing.css";
+
 
 // Put data to go in cards here
-const housingCardData = [
-    { id: 1, title: "Kensington Apartments", description: "this is the first card", image: "/house-pic.jpg" },
-    { id: 2, title: "Salvation Army Social Services Center", description: "this is the second card", image: "/house-pic.jpg" },
-    { id: 3, title: "Pathways at North Loop Apartments", description: "this is the third card", image: "/house-pic.jpg" },
+const HousingCardData = [
+    {   id: 1, 
+        name: "Kensington Apartments", 
+        cost: "",
+        rating: "",
+        HousingStyle: "",
+        address: "",
+        image: "https://rentpath-res.cloudinary.com/t_3x2_fixed_webp_xl/t_unpaid/e2335139f4a9f2257227377307f74af1"
+    },
+    {   id: 2, 
+        name: "Salvation Army Social Services Center",
+        cost: "",
+        rating: "",
+        HousingStyle: "",
+        address: "",
+        image: "https://lh3.googleusercontent.com/p/AF1QipMAAvugxD42xXAw5K-TPQM7RbRkxZRFJpZsfaqs=s1360-w1360-h1020"
+    },
+    {   id: 3, 
+        name: "Pathways at North Loop Apartments",
+        cost: "",
+        rating: "",
+        HousingStyle: "",
+        address: "",
+        image:  "https://www.hacanet.org/wp-content/uploads/2017/02/NorthLoop-06-1024x683.jpg"
+       }
   ];
+const pageTitle="Housing";
+const pageDescription= "Find affordable housing in your area";
+const cardData = HousingCardData;
 
 function Housing(){
-    return(
-    <>
-        <GeneralPage
-         pageTitle="Housing Services"
-         pageDescription= "Find the best housing services in your area"
-         cardData = {housingCardData}
-         />
-    </>);
+    return( 
+        <div className="housing-page-container ">
+            <h1 className="housing-page-title">{pageTitle}</h1>
+            <p className="housing-page-description">{pageDescription}</p>
+            <p className="housing-page-description">Showing 3/3 Instances</p>
+        {/* NEW WRAPPER FOR CARDS */}
+        <div className="cards-container">
+            {cardData.map((currCard) => (
+                <div key={currCard.id} style={{ width: "350px" }}> {/* Adjust width as needed */}
+                < HousingCard
+                    name={currCard.name} 
+                    image={currCard.image}
+                    id={currCard.id}
+                    cost={currCard.cost}
+                    rating={currCard.rating}
+                    HousingStyle={currCard.HousingStyle}
+                    Address={currCard.Address}
+                />
+                </div>
+            ))}
+        </div>
+    </div>
+);
 }
 
 export default Housing;

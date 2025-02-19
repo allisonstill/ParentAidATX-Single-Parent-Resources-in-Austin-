a86f145064693b1ac4495f6c55822a0b6f41c88a
Rubi Rojas <rubirojas@Rubis-Laptop.local>
2025-02-19 10:25:12 -0600

    added government programs

--- frontend/src/pages/Programs.jsx
+++ frontend/src/pages/Programs.jsx
@@ -2,9 +2,9 @@ import GeneralPage from "./GeneralPage"
 
 // Put data to go in cards here
 const governmentProgramCardData = [
-    { id: 1, title: "Temporary Assistance for Needy Families (TANF)", description: "this is the first card", image: "TANF.png" },
-    { id: 2, title: "Child Care Assistance Program (CCAP)", description: "this is the second card", image: "/house-pic.jpg" },
-    { id: 3, title: "Supplemental Nutrition Assistance Program (SNAP)", description: "this is the third card", image: "/house-pic.jpg" },
+    { id: 1, title: "Temporary Assistance for Needy Families (TANF)", description: "this is the first card", image: "/TANF.png" },
+    { id: 2, title: "Child Care Assistance Program (CCAP)", description: "this is the second card", image: "/cityofAustin.jpg" },
+    { id: 3, title: "Supplemental Nutrition Assistance Program (SNAP)", description: "this is the third card", image: "/snap.jpg" },
   ];
 
 function Housing(){

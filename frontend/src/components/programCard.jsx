4f7775ab7b2b52593e0d47822584797dd639e486
Rubi Rojas <rubirojas@Rubis-Laptop.local>
2025-02-19 17:22:37 -0600

    government page done

--- frontend/src/components/programCard.jsx
+++ frontend/src/components/programCard.jsx
@@ -11,10 +11,15 @@ function ProgramCard({ image, name, IDnum, description, website, govtScope, govt
       <div className="card-body">
         <h5 className="ProgramCard-title">{name}</h5>
         <p className="ProgramCard-text">{description}</p>
+        <p className="attribute-label">ID Number</p>
         <p className="ProgramCard-attributes">{IDnum}</p>
+        <p className="attribute-label">Website</p>
         <p className="ProgramCard-attributes">{website}</p>
+        <p className="attribute-label">Government Scope </p>
         <p className="ProgramCard-attributes">{govtScope}</p>
+        <p className="attribute-label">Government Admin </p>
         <p className="ProgramCard-attributes">{govtAdmin}</p>
+        <p className="attribute-label">Category </p>
         <p className="ProgramCard-attributes">{cat}</p>
         <a href="/housing" className="custom-btn">Go somewhere</a>
       </div>
--- frontend/src/pages/Programs.jsx
+++ frontend/src/pages/Programs.jsx
@@ -6,7 +6,7 @@ import "./Programs.css";
 const governmentProgramCardData = [
     {   id: 1, 
         name: "Temporary Assistance for Needy Families (TANF)", 
-        IDnum: "Program ID: 1",
+        IDnum: "1",
         description: "The Temporary Assistance for Needy Families program is designed to help families with children experiencing low-income achieve economic security and stability.", 
         website: "https://yourtexasbenefits.com/Learn/Home/",
         govtScope: "Texas", 
@@ -16,22 +16,22 @@ const governmentProgramCardData = [
     },
     {   id: 2, 
         name: "Child Care Assistance Program (CCAP)",
-        IDnum: "12345",
-        description: "this is the second card", 
-        website: " ",
-        govtScope: " ",
-        govtAdmin: " ",
-        cat: " ",
+        IDnum: "2",
+        description: "Child care assistance programs help families pay for child care so they can work, study, or train for jobs. These programs are funded by the federal government and may also be called vouchers, subsidies, or fee assistance.", 
+        website: "https://www.austintexas.gov/department/child-care/",
+        govtScope: "Texas",
+        govtAdmin: "Texas Workforce Commission",
+        cat: "Child care",
         image: "/cityofAustin.jpg" 
     },
     {   id: 3, 
         name: "Supplemental Nutrition Assistance Program (SNAP)",
-        IDnum: " ",
-        description: "this is the third card",
-        website: " ",
-        govtScope: " ",
-        govtAdmin: " ",
-        cat: " ", 
+        IDnum: "3",
+        description: "SNAP provides food benefits to low-income families to supplement their grocery budget so they can afford the nutritious food essential to health and well-being.",
+        website: "https://www.fns.usda.gov/snap/supplemental-nutrition-assistance-program/",
+        govtScope: "Texas",
+        govtAdmin: "US Department of Agriculture",
+        cat: "Food", 
         image: "/snap.jpg" }
   ];
 const pageTitle="Government Services";
@@ -43,6 +43,7 @@ function Programs(){
         <div className="programs-page-container">
             <h1 className="programs-page-title">{pageTitle}</h1>
             <p className="programs-page-description">{pageDescription}</p>
+            <p className="programs-page-description">Showing 3/3 Instances</p>
         {/* NEW WRAPPER FOR CARDS */}
         <div className="cards-container">
             {cardData.map((currCard) => (

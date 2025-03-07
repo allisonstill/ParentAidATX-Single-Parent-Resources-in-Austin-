d9e11cb95ad08e1b579469f2a1cf4a6024574343
Andrew Harvey <andrewharvey504@gmail.com>
2025-03-06 20:17:42 -0600

    working on api endpoints and defining models with postgreSQL

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,36 +1,28 @@
-"""
-This file contains our API implementation.
-"""
-from typing import List, Dict
-from flask import Flask
-import mysql.connector
-import json
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allows frontend to access backend
 
+# Connect to MySQL or PostgreSQL
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-def favorite_colors() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'knights'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM favorite_colors')
-    results = [{name: color} for (name, color) in cursor]
-    cursor.close()
-    connection.close()
+db = SQLAlchemy(app)
 
-    return results
+# Define model
+class GovernmentProgram(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(500), nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
+# API endpoint: Get all programs
+@app.route("/api/programs", methods=["GET"])
+def get_programs(): # runs this method when a GET request is made to the api/programs
+    programs = GovernmentProgram.query.all() # query the database to get all government programs (gets all rows)
+    return jsonify([{"id": p.id, "name": p.name, "url": p.url, "description": p.description} for p in programs]) # return queried data as a JSON
 
-@app.route('/')
-def index() -> str:
-    return json.dumps({'favorite_colors': favorite_colors()})
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)

1d11bce1674a2b59d56759e9c0c3f915a6a35287
Andrew Harvey <andrewharvey504@gmail.com>
2025-03-10 22:10:10 -0500

    added display number of instances to top of childcare page

--- frontend/src/components/childCard.jsx
+++ frontend/src/components/childCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./childCard.css";
 
 
-function ChildCard({ image, name, type, rating, cost, Address, website, id}) {
+function ChildCard({ image, name, type, age_range, open_time, close_time, address, id}) {
   const handleClick = () => {
     window.scrollTo(0, 0);
   };
@@ -16,16 +16,18 @@ function ChildCard({ image, name, type, rating, cost, Address, website, id}) {
       />
       <div className="card-body">
         <h5 className="ChildCard-title">{name}</h5>
-        <p className="attribute-label">Cost</p>
-        <p className="ChildCard-attributes">{cost}</p>
         <p className="attribute-label">Type</p>
         <p className="ChildCard-attributes">{type}</p>
-        <p className="attribute-label">Rating</p>
-        <p className="ChildCard-attributes">{rating}</p>
+        <p className="attribute-label">Age Range</p>
+        <p className="ChildCard-attributes">{age_range}</p>
+        <p className="attribute-label">Open Time</p>
+        <p className="ChildCard-attributes">{open_time}</p>
+        <p className="attribute-label">Close Time</p>
+        <p className="ChildCard-attributes">{close_time}</p>
         <p className="attribute-label">Address</p>
-        <p className="ChildCard-attributes">{Address}</p>
-        <p className="attribute-label">Website</p>
-        <p className="ChildCard-attributes">{website}</p>
+        <p className="ChildCard-attributes">{address}</p>
+        {/*<p className="attribute-label">Website</p>
+        <p className="ChildCard-attributes">{website}</p>*/}
         <Link to={`/childcare/${id}`} className="custom-btn" onClick={handleClick}>
           View Details
         </Link>
--- frontend/src/pages/Childcare.jsx
+++ frontend/src/pages/Childcare.jsx
@@ -1,59 +1,47 @@
 import "./Childcare.css";
 import ChildCard from "../components/childCard";
+import React, { useEffect, useState } from "react";
 
-// Put data to go in cards here
-const childcareCardData = [
-  {   id: 1,
-      name: "Child Craft Schools", 
-      type: "Daycare",
-      cost: "850-1050",
-      rating: "5.0",
-      Address: "800 W 30th St, Austin, TX 78705",
-      website: "https://www.childcraftschooltx.com/index.html",
-      image: "https://childcraftschooltx.com/uploads/1/2/3/5/123531586/published/image_6.png?1546248481"
-  },
-  {   id: 2, 
-      name: "First English Lutheran Child Development Center",
-      type: "Daycare",
-      cost: "N/A",
-      rating: "5.0",
-      Address: "3001 Whitis Ave, Austin, TX 78705",
-      website: "https://www.firstenglishcdc.org/",
-      image: "https://images.squarespace-cdn.com/content/v1/659d9f977b7a4100052c42be/1704828826252-OLIWM0453MDOXDWKZAOY/FELCDC_Food.png"
-  },
-  {   id: 3, 
-      name: "Lil' Angels Daycare Center",
-      type: "Daycare",
-      cost: "N/A",
-      rating: "4.9",
-      Address: "6006 Cameron Rd, Austin, TX 78723",
-      website: "http://lilangelsaustin.com/",
-      image:  "https://winnie.imgix.net/30236f96-99fe-403a-aa72-5e38719cbab7?w=242&h=124&dpr=3&fit=crop&auto=compress"
-   }
-];
 const pageTitle="Child Care Services";
 const pageDescription= "Find affordable child care services in your area";
-const cardData = childcareCardData;
 
 function Childcare(){
+
+  const [daycares, setDaycares] = useState([]); // the api response data will be stored in "daycares"
+  // Fetch data for childcare using API
+  useEffect(() => {
+      const fetchDaycares = async () => {
+          try {
+              const response = await fetch("https://flask-api-production-730f.up.railway.app/api/childcare");
+              const data = await response.json();
+              console.log("Fetched data:", data); // Debugging (F12)
+              setDaycares(data);
+          } catch (error) {
+              console.error("Error fetching daycares:", error);
+          }
+      };
+
+      fetchDaycares();
+    }, []);
+
     return(
       <div className="child-page-container">
         <h1 className="child-page-title">{pageTitle}</h1>
         <p className="child-page-description">{pageDescription}</p>
-        <p className="child-page-description">Showing 3/3 Instances</p>
-        {/* NEW WRAPPER FOR CARDS */}
+        <p className="child-page-description">Showing {daycares.length} {daycares.length === 1 ? "Instance" : "Instances"}</p>
         <div className="ChildCards-container">
-          {cardData.map((currCard) => (
-            <div key={currCard.id} style={{width: "350px"}}> {/* Adjust width as needed */}
+          {daycares.map((daycare) => (
+            <div key={daycare.id} style={{width: "350px"}}> {/* Adjust width as needed */}
               < ChildCard
-                  name={currCard.name}
-                  image={currCard.image}
-                  id={currCard.id}
-                  type={currCard.type}
-                  cost={currCard.cost}
-                  rating={currCard.rating}
-                  Address={currCard.Address}
-                  website={currCard.website}
+                  image={daycare.image_url}
+                  name={daycare.name}
+                  type={daycare.program_type}
+                  age_range={daycare.age_range}
+                  open_time={daycare.open_time}
+                  close_time={daycare.close_time}
+                  address={daycare.address}
+                  /*website={daycare.full_link} I think this should go on the instance page, not the card.*/
+                  id={daycare.id}
               />
               </div>
           ))}

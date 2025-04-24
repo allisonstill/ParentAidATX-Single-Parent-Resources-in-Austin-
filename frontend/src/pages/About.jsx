ad13bb56aca0eaef323030c888a8af89844477e6
Rubi Rojas <rubirojas@Rubis-Laptop.local>
2025-04-23 20:43:07 -0500

    self critique

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -68,6 +68,9 @@ function About() {
       <p className="about-description">
         <span className="fw-bold">API Documentation Link: </span> <a href="https://documenter.getpostman.com/view/42442568/2sAYdZstBv"> Documentation </a>
       </p>
+      <p className="about-description">
+        <span className="fw-bold">GitLab Link: </span> <a href="https://gitlab.com/aharvey502/cs373-spring-2025-group-17/"> Link </a>
+      </p>
       <h3 className="about-subtitle mt-5 mb-3">Meet The Team</h3>
 
       <div className="d-flex flex-wrap justify-content-center gap-5">
@@ -82,42 +85,40 @@ function About() {
         housing options and childcare services can be overwhelming and time-consuming for single parents.
         We aim to simplify this process by consolidating the most useful resources into a single, accessible website.</p>
         <div className="self-critique-section mt-5 p-4 border-top">
-        <h2 className="mb-4">Critique & Reflection</h2>
+        <h2 className="mb-10">Critique & Reflection</h2>
         <h4>Self Critique</h4>
         <div className="space-y-4">
             <div>
-              <strong>What did they do well?</strong><br />
-              answer
+              <strong>What did we do well?</strong><br />
+              We partook in frequent communication, collaboration, and delegation of tasks, which allowed us to have strong team synergy and complete the project efficiently.
             </div>
 
             <div>
-              <strong>How effective was their RESTful API?</strong><br />
-              answer
+              <strong>What did we learn?</strong><br />
+              We learned a plethora of technical skills, including CI/CD workflows, React, APIs, Database Management, AWS, JavaScript, HTML, CSS, and Python. We also learned how to design, structure, and refactor a large code base so it scales and stays maintainable. Most importantly, we learned the importance of clear communication and organization.
             </div>
 
             <div>
-              <strong>How well did they implement your user stories?</strong><br />
-              answer
+              <strong>What did we teach each other?</strong><br />
+              Each member brought unique strengths that improved everyone’s technical knowledge. We also taught each other best coding practices, like how to reuse code with React components, React props, database management, and safe API key management.
             </div>
 
             <div>
-              <strong>What did we learn from their website?</strong><br />
-              answer
+              <strong>What can we do better?</strong><br />
+              Our website could have benefited from additional rounds of refactoring. As the semester progressed, we became more focused on implementing features and meeting deadlines, which occasionally came at the expense of writing clean, maintainable code. In retrospect, establishing a more consistent code structure and adhering to clear naming conventions from the beginning would have improved both readability and scalability.
             </div>
 
             <div>
-              <strong>What can they do better?</strong><br />
-              answer
+              <strong>What effect did the peer reviews have?</strong><br />
+              The peer reviews provided encouragement and constructive feedback for each team member, helping us build a culture of support.
             </div>
 
             <div>
-              <strong>What puzzles us about their website?</strong><br />
-              answer
+              <strong>What puzzles us?</strong><br />
+              We are still puzzled about the AWS infrastructure, especially with sharing an EC2 instance across multiple users/developers. Additionally, we ran into some issues refactoring our code, particularly when refactoring came at the expense of functional code.
             </div>
           </div>
-
-        <h4 className="d-flex align-items-center gap-3 justify-content-start">UTherapy Critique
-        </h4>
+        <h4 className="mt-16 d-flex align-items-center gap-3 justify-content-start">UTherapy Critique</h4>
         <div className="space-y-4">
           <div>
             <strong>What did they do well?</strong><br />
